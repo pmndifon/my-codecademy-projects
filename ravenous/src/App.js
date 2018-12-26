0efb2b5ed2588e05323d906ac9e2ef7a843cd9d2
Patrick Ndifon <pmndifon@gmail.com>
2018-12-26 22:01:35 +0100

    Refactor code to use props

--- ravenous/src/App.js
+++ ravenous/src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import './App.css';
+
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
+
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
 
 class App extends React.Component {
   render() {
@@ -9,7 +31,7 @@ class App extends React.Component {
       <div className="App">
         <h1>Ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses}/>
       </div>
     );
   }
